c49d5553454fd506078c13c932dea0e9b4d2d4a3
Andrya <palinskiandrya@gmail.com>
2023-07-21 20:05:05 -0300

    until now it was working

--- crudApiJson/src/controller/userController.ts
+++ crudApiJson/src/controller/userController.ts
@@ -11,6 +11,68 @@ async function listenUsers (req: Request, res: Response) {
 
 };
 
+
+async function postUsers (req: Request, res: Response) {
+    const jsonDataBase = fs.readFileSync(data); 
+    let content = JSON.parse(jsonDataBase);
+    //Verifica a quantidade de objetos na base de dados.
+    let index: number = Object.keys(content).length;
+    
+    content[index++] = req.body;
+    const values = JSON.stringify(content);
+   
+    fs.writeFileSync(data, values)
+   
+   res.status(201).send("User registered succesfully! :D")
+    //console.log(req.body); <-Testes
+    //res.send(index);
+};
+
+async function putUser(req: Request, res: Response) {
+  //Atribui a base de dados em nova variavel. 
+  const jsonDataBase = fs.readFileSync(data);
+
+  const userId = req.params.id; 
+  let content = JSON.parse(jsonDataBase);
+
+  content[userId] = req.body;
+  
+  const values = JSON.stringify(content);
+ 
+  fs.writeFileSync(data, values)
+ 
+  res.status(201).send(`Updated user ${userId} :>`)
+
+};
+
+async function deletUser (req: Request, res: Response) {
+//atribui a base de dados em nova variavel
+const jsonDataBase = fs.readFileSync(data);
+
+//recupera o id enviado por parametro
+const userId = req.params.id;
+
+//analisa string JSON e transforma em um objeto JavaScript
+let content = JSON.parse(jsonDataBase);
+
+//delete
+delete content[userId];
+
+//analisa um objeto em JavaScript e transforma em uma string JSON
+const values = JSON.stringify(content);
+
+//lê o arquivo da base de dados e adiciona o novo objeto
+fs.writeFileSync(data, values);
+
+//retorno amigável para o usuário que o endpoint
+res.send(`User with id ${userId} has been deleted ;--;`);
+
+};
+
+
 export default {
-    listenUsers,
+    listenUsers, 
+    postUsers,
+    putUser,
+    deletUser
 }
--- crudApiJson/src/routes/userRouter.ts
+++ crudApiJson/src/routes/userRouter.ts
@@ -9,65 +9,13 @@ const router = express.Router();
 //Listar usuários. 
 router.get('/users', userController.listenUsers);
 
-
-/*
 //Cadastrar usuários.
-router.post('/users', (req: any, res: any)=>{
-    const jsonDataBase = fs.readFileSync(data); 
-    let content = JSON.parse(jsonDataBase);
-    //Verifica a quantidade de objetos na base de dados.
-    let index: number = Object.keys(content).length;
-    
-    content[index++] = req.body;
-    const values = JSON.stringify(content);
-   
-    fs.writeFileSync(data, values)
-   
-   res.status(201).send("User registered succesfully! :D")
-    //console.log(req.body); <-Testes
-    //res.send(index);
-});
+router.post('/users', userController.postUsers);
 
 //Atualizar usuário.
-router.put('/user/:id', (req: any, res: any)=>{
-    //Atribui a base de dados em nova variavel. 
-    const jsonDataBase = fs.readFileSync(data);
-
-    const userId = req.params.id; 
-    let content = JSON.parse(jsonDataBase);
-
-    content[userId] = req.body;
-    
-    const values = JSON.stringify(content);
-   
-    fs.writeFileSync(data, values)
-   
-    res.status(201).send(`Updated user ${userId} :>`)
-
-});
+router.put('/user/:id', userController.putUser);
 
 //Deletar usuário.
-router.delete('/user/:id', (req: any, res: any) => {
-    //atribui a base de dados em nova variavel
-    const jsonDataBase = fs.readFileSync(data);
+router.delete('/user/:id', userController.deletUser);
 
-    //recupera o id enviado por parametro
-    const userId = req.params.id;
-    
-    //analisa string JSON e transforma em um objeto JavaScript
-    let content = JSON.parse(jsonDataBase);
-    
-    //delete
-    delete content[userId];
-    
-    //analisa um objeto em JavaScript e transforma em uma string JSON
-    const values = JSON.stringify(content);
-
-    //lê o arquivo da base de dados e adiciona o novo objeto
-    fs.writeFileSync(data, values);
-
-    //retorno amigável para o usuário que o endpoint
-    res.send(`User with id ${userId} has been deleted ;--;`);
-});
-*/
 export default router;
